5be07ede0f353d71d30ecf5a6625c786fa4e1cf9
DiegoEdi <demoncada0@soy.sena.edu.co>
2024-07-04 20:07:14 -0500

    nuevos cambios BISIGOIFFMM

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+ //importamos los controladores
+use App\Http\Controllers\ArticleControlle;
+use App\Http\Controllers\CatecoryController;
+use App\Http\Controllers\Income_detailController;
+use App\Http\Controllers\IncomeController;
+use App\Http\Controllers\PersonController;
+use App\Http\Controllers\Sale_detailController;
+use App\Http\Controllers\SaleController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +24,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+});  
+
+Route::resourse('Articles',ArticleControlle::class);
 
 require __DIR__.'/auth.php';
