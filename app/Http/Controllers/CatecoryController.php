cff927babb8d91249d2abc8b8df28d8197bf7344
DiegoEdi <demoncada0@soy.sena.edu.co>
2024-07-07 02:59:05 -0500

    Nuevos Cambios BISIGOIFFMM

--- app/Http/Controllers/CatecoryController.php
+++ app/Http/Controllers/CatecoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category; 
 
 class CatecoryController extends Controller
 {
@@ -11,7 +12,8 @@ class CatecoryController extends Controller
      */
     public function index()
     {
-        return view ('dashboard.category.index');
+        $category=Category::all();
+        return view ('dashboard.category.index',['category'=>$category]);
     }
 
     /**
